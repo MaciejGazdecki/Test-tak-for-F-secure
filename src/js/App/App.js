f3fd9498bd4dfb1c01353b04e08b1812e241a826
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2020-01-20 19:09:19 +0100

    Navigation bar done

--- src/js/App/App.js
+++ src/js/App/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactDOM from "react-dom";
+import {HashRouter as Router, Route,Switch} from "react-router-dom";
+import Navigation from "../Navigation/navigation";
 
 
 function App () {
     return (
-        <div>React</div>
+        <Router>
+            <Navigation/>
+        </Router>
     )
 }
 
