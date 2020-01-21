e7d02589cc24d232e6f2e3aa8beda9af1269c2c8
Maciej Gazdecki <maciej.gazdecki@gmail.com>
2020-01-21 02:58:01 +0100

    I added filter option to select

--- src/js/App/App.js
+++ src/js/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {HashRouter as Router, Route,Switch} from "react-router-dom";
 import Navigation from "../Navigation/navigation";
 import Header from "../upperBar/header";
@@ -9,21 +9,28 @@ import FeatureFour from "../Features/FeatureFour/featureFour";
 import FeatureFive from "../Features/FeatureFive/featureFive";
 import Another from "../Features/Another/another";
 import YetAnother from "../Features/yetAnother/yetAnother";
+import {OptionContext} from "./optionContext";
+import {SetOptionContext} from "./setOptionContext";
 
 function App () {
+    const [option, setOption] = useState('');
     return (
         <Router>
-            <Header/>
-            <Navigation/>
-            <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route path='/feature-two' component={FeatureTwo}/>
-                <Route path="/feature-three" component={FeatureThree}/>
-                <Route path="/feature-four" component={FeatureFour}/>
-                <Route path="/feature-five" component={FeatureFive}/>
-                <Route path="/another" component={Another}/>
-                <Route path="/yet-another" component={YetAnother}/>
-            </Switch>
+            <OptionContext.Provider value={option}>
+                <SetOptionContext.Provider value={setOption}>
+                    <Header/>
+                    <Navigation/>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route path='/feature-two' component={FeatureTwo}/>
+                        <Route path="/feature-three" component={FeatureThree}/>
+                        <Route path="/feature-four" component={FeatureFour}/>
+                        <Route path="/feature-five" component={FeatureFive}/>
+                        <Route path="/another" component={Another}/>
+                        <Route path="/yet-another" component={YetAnother}/>
+                    </Switch>
+                </SetOptionContext.Provider>
+            </OptionContext.Provider>
         </Router>
     )
 }
